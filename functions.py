b34579410fd6b1061db195e94ddd3d5af1424d1c
Phantom <abracadabra1253@gmail.com>
2024-03-20 17:17:24 +0400

    Escaping function for html pages

---
+++ functions.py
@@ -0,0 +1,31 @@
+def escape(text: str):
+
+	"""
+
+		<: &lt;
+		>: &gt;
+		\: &#92;
+		/: &#47;
+		': &apos; or &#39;
+		": &quot; or &#34;
+
+	"""
+		
+	REPLACEMENTS = [
+		("\\", "&#92;"),
+		("/", "&#47;"),
+		("'", "&#39;"),
+		("\"", "&#34;"),
+		("<", "&lt;"),
+		(">", "&gt;"),
+	]
+	
+	
+	for x,y in REPLACEMENTS:
+		text = text.replace(x,y)
+	
+	print(text)
+	
+
+
+escape("test '   \" ")
