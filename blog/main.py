89cc7adbfb96505ac114e53095f9cffa3da6f9b4
Phantom <abracadabra1253@gmail.com>
2024-03-23 20:57:01 +0400

    ...

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-
+from flask import  render_template
 
 app2Blg2 = Blueprint("hop_hip_bux", __name__, template_folder = "templates")
 
@@ -7,7 +7,7 @@ app2Blg2 = Blueprint("hop_hip_bux", __name__, template_folder = "templates")
 
 @app2Blg2.route("/blog/", methods=["GET"])
 def FromTheBlog():
-	return "main blogs"
+	return render_template("blog/main.html")
 
 
 @app2Blg2.route("/blog/post/<int:uid>", methods=["GET"])
