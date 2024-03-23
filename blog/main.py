89ddad5cc518047960aa81b5f7fd7bf399671692
Phantom <abracadabra1253@gmail.com>
2024-03-23 22:43:56 +0400

    ..

--- blog/main.py
+++ blog/main.py
@@ -6,6 +6,7 @@ app2Blg2 = Blueprint("hop_hip_bux", __name__, template_folder = "templates")
 
 @app2Blg2.route("/blog/", methods=["GET"])
 def FromTheBlog():
+	#abort(404)
 	return render_template("blog/main.html")
 
 
