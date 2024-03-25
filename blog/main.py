c3b4d1e9abd220f82ca043ba9f11420caeaabeee
Phantom <abracadabra1253@gmail.com>
2024-03-25 20:31:03 +0400

    Some Changes for index

--- blog/main.py
+++ blog/main.py
@@ -6,8 +6,60 @@ app2Blg2 = Blueprint("hop_hip_bux", __name__, template_folder = "templates")
 
 @app2Blg2.route("/blog/", methods=["GET"])
 def FromTheBlog():
-	#abort(404)
-	return render_template("blog/main.html")
+	
+		FData = [
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+		]
+	
+		Fordinary = [
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '15','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '16','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'}, 
+			{'name':'qweqwe',	'buh': 'kekz',	'num': '17','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},  
+			{'name':'qweqwe22','buh': 'kekz33',	'num': '18','pic':'/static/apartment/cbd17284cdabb02e39989c72aadba5fb4457984017.jpeg'},
+		]
+	
+		#abort(404)
+		return render_template("blog/main.html" ,Data=FData, Ford = Fordinary)
 
 
 @app2Blg2.route("/blog/post/<int:uid>", methods=["GET"])
